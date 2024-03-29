466ab7494fc61236126820775a631353fb2d939c
ElMouddenRiad <137223168+ElMouddenRiad@users.noreply.github.com>
2024-03-29 01:49:25 +0000

    dal2

--- financePredict_back-end/financePredict/src/main/java/com/pfa/financePredict/service/UserService.java
+++ financePredict_back-end/financePredict/src/main/java/com/pfa/financePredict/service/UserService.java
@@ -10,13 +10,27 @@ import java.util.Optional;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
-
-    public User saveUser(User user) {
-        return userRepository.save(user);
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
