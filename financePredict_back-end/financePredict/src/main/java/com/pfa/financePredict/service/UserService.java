52c73621036e45b82746401213c46a5d7110dbf1
bakr oukessou <bakr.oukessou@gmail.com>
2024-03-29 22:26:52 +0000

    error handling with success

--- financePredict_back-end/financePredict/src/main/java/com/pfa/financePredict/service/UserService.java
+++ financePredict_back-end/financePredict/src/main/java/com/pfa/financePredict/service/UserService.java
@@ -4,12 +4,15 @@ import com.pfa.financePredict.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
+    private UserRepository userRepository;
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -21,11 +24,12 @@ public class UserService {
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
-    
-    public User createUser(User user) {
+
+    public User saveUser(User user) {
         return userRepository.save(user);
     }
-    
+
+
     public User updateUser(User user) {
         return userRepository.save(user);
     }
@@ -33,4 +37,8 @@ public class UserService {
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
 }
